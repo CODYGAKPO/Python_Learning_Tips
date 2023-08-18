c761e85848d3aff875a811df5e9e146b57ad3582
CODYGAKPO <129563577+CODYGAKPO@users.noreply.github.com>
2023-08-18 00:16:02 +0000

    Uncommented main.py prints

--- main.py
+++ main.py
@@ -15,20 +15,19 @@ nikhils_bool = False
 ericas_string = """a nice mom"""
 nikhils_string = "This is nikhil's string"
 
-
 print("This is Aidens number", aidens_number)
 print("This is Nikhils number", nikhils_number)
 
 print('This statement is...', nikhils_bool)
 print('This statement is...', aidens_bool)
 
-print('Erica is',ericas_string)
+print('Erica is', ericas_string)
 print("this is nikhils_string", nikhils_string)
 
 # Sequence Data types
 # List value type
 # You use it to store values that are of the same type: homogenous
-meghas_list = [1,2,3,45,66,70]
+meghas_list = [1, 2, 3, 45, 66, 70]
 
 # Tuple value type
 # You use this to store values of different types (heterogeneous)
@@ -39,34 +38,33 @@ ishwar_tuple = ('hi grandpa', 'how are you?')
 # Convenient way to create a sequence that is a range of values
 aiden_range = range(1, 11)
 shawn_range = range(1, 11, 4)
-  
-
 
 print('The three numbers are...', meghas_list)
 
 print(ishwar_tuple)
 print("Name and Age: ", name_age)
 
+print(list(aiden_range))
+print(list(shawn_range))
 
-# print(list(aiden_range))
-# print(list(shawn_range))
+aiden_var = '345'
+print('Value of aiden_var: ', aiden_var, 'The Type of aiden_var: ',
+      type(aiden_var))
 
+aiden_var = int(aiden_var)
+print('Value of aiden_var: ', aiden_var, 'The Type of aiden_var: ',
+      type(aiden_var))
 
-# aiden_var = '345'
-# print('Value of aiden_var: ', aiden_var, 'The Type of aiden_var: ', type(aiden_var))
+aiden_var = float(aiden_var)
+print('Value of aiden_var: ', aiden_var, 'The Type of aiden_var: ',
+      type(aiden_var))
 
-# aiden_var = int(aiden_var)
-# print('Value of aiden_var: ', aiden_var, 'The Type of aiden_var: ', type(aiden_var))
+aiden_var = str(aiden_var)
+print('Value of aiden_var: ', aiden_var, 'The Type of aiden_var: ',
+      type(aiden_var))
 
-# aiden_var = float(aiden_var)
-# print('Value of aiden_var: ', aiden_var, 'The Type of aiden_var: ', type(aiden_var))
-
-# aiden_var = str(aiden_var)
-# print('Value of aiden_var: ', aiden_var, 'The Type of aiden_var: ', type(aiden_var))
-
-# result = aiden_var + 10
-# print("The result of the operation of addint 10 to aiden_var is: ", result)
-
+result = aiden_var + 10
+print("The result of the operation of addint 10 to aiden_var is: ", result)
 
 var_1 = 123
 var_2 = 345
